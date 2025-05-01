b91857480ea9aa6f917a572633c001ac91ec8fe3
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2025-05-01 14:38:32 +0300

    login update

--- XFitGym/XFitGym.cpp
+++ XFitGym/XFitGym.cpp
@@ -6,6 +6,13 @@ XFitGym::XFitGym(QWidget *parent)
     ui.setupUi(this);
     log = new Login(this);
     ui.Main->addWidget(log);
+    connect(log->ui.Login, &QPushButton::clicked, this, [=]() {
+        log->ui.Email->setText("");
+        log->ui.Password->setText("");
+        qDebug() << "LogIn";
+        });
+    connect(log->ui.Exit, &QPushButton::clicked, this, QApplication::quit);
+
 }
 
 XFitGym::~XFitGym()
