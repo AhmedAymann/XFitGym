4cb75568294dd1e3cb3c330562f6556347054076
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2025-05-06 01:02:57 +0300

    User GUI Finished (padel&clases)

--- XFitGym/XFitGym.h
+++ XFitGym/XFitGym.h
@@ -1,17 +1,19 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QString>
+#include <QScrollBar>
+#include "qtimer.h"
 #include "ui_XFitGym.h"
 #include "Login.h"
 #include "Dashboard.h"
 #include "Manager.h"
 #include "Homepage.h"
 #include "userProfile.h"
+#include "classes.h"
 #include "notifications.h"
 #include "feedback.h"
-#include <QString>
-#include <QScrollBar>
-#include "qtimer.h"
+#include "Padel.h"
 
 
 class XFitGym : public QMainWindow
@@ -28,6 +30,8 @@ Homepage* home;
 userProfile* user_Profile;
 Notifications* notifications;
 Feedback* feedback;
+Classes* classes;
+Padel* padel;
 
     XFitGym(QWidget *parent = nullptr);
     ~XFitGym();
@@ -37,4 +41,20 @@ Feedback* feedback;
     void load();
 private:
     Ui::XFitGymClass ui;
+};
+
+
+
+class Cards : public QWidget {
+    Q_OBJECT
+public:
+    Cards(QString title, QString line1, QString line2, QWidget* parent = nullptr);
+    //for classes
+    Cards(QString title, QString line1, QString line2, int attendees, int max, QWidget* parent = nullptr);
+    QHBoxLayout* buttonLayout;
+
+private:
+    QLabel* titleLabel;
+    QLabel* line1Label;
+    QLabel* line2Label;
 };
