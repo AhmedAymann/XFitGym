c0fdb73a50b0718c126cea37983841b06ff3632a
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2025-05-11 07:16:56 +0300

    ferkesh GUI (rest of manager and all coach and receptionist)

--- XFitGym/Manager_members.h
+++ XFitGym/Manager_members.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QtWidgets/QMainWindow>
 #include "ui_manager_members.h"
+#include "ui_renewPage.h"
 class Manager_members : public QWidget
 {
     Q_OBJECT
@@ -11,3 +12,13 @@ public:
     Ui::Manager_membersClass ui;
 };
 
+class renewMembers : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    renewMembers(QWidget* parent = nullptr);
+    ~renewMembers();
+    Ui::renewMembersClass ui;
+
+};
